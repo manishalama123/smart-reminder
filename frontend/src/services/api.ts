62083aa29afcbb7b19699088c03fbada508b896d
Manisha Lama <manishalama136@gmail.com>
2025-10-13 12:56:35 +0545

    api handlineg: base url setup , interceptor, authAPI and reminderAPI

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import type { AuthTokens, LoginCredentials, RegisterData, User, Reminder } from '@/types';
+
+const API_BASE_URL = 'http://localhost:8000/api';
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem('access_token');
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+// Auth API
+export const authAPI ={
+    register: (data: RegisterData)=>
+        api.post<User>('/users/register/', data),
+
+    login: (credential: LoginCredentials)=>
+        api.post<AuthTokens>('/users/loging/', credential),
+
+    refreshToken: (refresh:string)=>
+        api.post<{access:string}>('/users/token/refresh', {refresh}),
+
+    getCurrentUser: ()=>
+        api.get<User>('/users/me'),
+    
+};
+
+// Reminders API
+export const reminderAPI = {
+    getAll:() =>
+        api.get<Reminder>('/reminders'),
+
+    getOne: (id: number)=>
+        api.get<Reminder>(`/reminders/${id}`),
+
+    create: (data: Partial<Reminder>)=>
+        api.post<Reminder>('/reminders/', data),
+
+    update: (id:number, data: Partial<Reminder>)=>
+        api.put<Reminder>(`/reminder/${id}/`, data),
+
+    delete: (id: number)=>
+        api.delete(`/reminders/${id}/`)
+    
+}
+export default api;
