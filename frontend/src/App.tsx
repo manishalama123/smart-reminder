01bdd933f033635456ff07058b22bc39eaeef75a
Manisha Lama <manishalama136@gmail.com>
2025-10-15 06:37:13 +0545

    Public and Protected Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import NotFound from './pages/NotFound'
+import { useAuthStore } from './stores/authStore'
 
+// Protected Route component
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated, loading } = useAuthStore();
+
+  if (loading) {
+    return (
+      <div className='min-h-screen flex items-center justify-center'>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+  return isAuthenticated ? <>{children}</> : <Navigate to='/login' />
+}
+
+// Public Route ( redirect to dashboard if already logged in)
+function PublicRoute({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated, loading } = useAuthStore();
+
+  if (loading) {
+    return (
+      <div className='min-h-screen flex items-center justify-center'>
+        <p>Loading...</p>
+      </div>
+    );
+
+  }
+  return isAuthenticated ? <Navigate to='/dashboard' /> : <>{children}</>
+}
 function App() {
-  
+  const initAuth = useAuthStore((state) => state.initAuth);
+
+  useEffect(() => {
+    initAuth();
+  }, [initAuth])
 
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/notfound" element={<NotFound/>} />      
+        <Route path='/' element={<Navigate to='/dashboard' />} />
+        <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
+        <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
