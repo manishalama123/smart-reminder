b46f0235e3c5e570b67041c35b35a3484de393ab
Manisha Lama <manishalama136@gmail.com>
2025-10-29 09:30:12 +0545

    email validation added

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError as DjangoValidationError
 from .models import User
 
 User= get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'timezone']
@@ -14,10 +16,22 @@ class UserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     password2 = serializers.CharField(write_only=True, label="Confirm Password")
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password','password2', 'timezone']
+    
+    def validate_email(self, value):
+        validator = EmailValidator()
+        try:
+            validator(value)
+        except DjangoValidationError:
+            raise serializers.ValidationError("Enter a valid email address.")
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("This email is already registered.")
+        
+        return value.lower()
     
     def validate(self, data):
         if data['password'] != data['password2']:
